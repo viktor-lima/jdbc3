8eda665206830dbe5d8e8a8020c6f4459ebefc53
viktor-lima <joaolimak10@outlook.com>
2021-03-10 14:47:29 -0300

    inserção com recuperação de Id

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,9 @@ package application;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -25,7 +27,8 @@ public class Program {
 						"insert into seller "
 						+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
 						+ "values"
-						+ "(?,?,?,?,?)"
+						+ "(?,?,?,?,?)",
+						Statement.RETURN_GENERATED_KEYS
 					);
 			
 			st.setString(1, "Carl Purple");
@@ -37,7 +40,17 @@ public class Program {
 			
 			int rowsAffected = st.executeUpdate();
 			
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			}
+			else {
+				System.out.println("No rows affected!");
+			}
+		
 			
 		}
 		catch (SQLException e) {
