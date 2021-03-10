06d8294b92fc7a9ab12405529e9a87574c3babea
viktor-lima <joaolimak10@outlook.com>
2021-03-10 15:06:16 -0300

    insert department

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,56 +13,37 @@ import db.DB;
 
 public class Program {
 	public static void main(String[] args) {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		Connection conn = null;
 		PreparedStatement st = null;
-		
+
 		try {
-			
+
 			conn = DB.getConnection();
-			
-			st = conn.prepareStatement(
-						"insert into seller "
-						+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-						+ "values"
-						+ "(?,?,?,?,?)",
-						Statement.RETURN_GENERATED_KEYS
-					);
-			
-			st.setString(1, "Carl Purple");
-			st.setString(2, "carl@gmail.com");
-			st.setDate(3, new Date(sdf.parse("22/04/1985").getTime()));
-			st.setDouble(4, 3000.0);
-			st.setInt(5, 4);
-			
-			
+
+			st = conn.prepareStatement("insert into department (Name) values ('D1'), ('D2')",
+					Statement.RETURN_GENERATED_KEYS);
+
 			int rowsAffected = st.executeUpdate();
-			
+
 			if (rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
-				while(rs.next()) {
+				while (rs.next()) {
 					int id = rs.getInt(1);
 					System.out.println("Done! Id = " + id);
 				}
-			}
-			else {
+			} else {
 				System.out.println("No rows affected!");
 			}
-		
-			
-		}
-		catch (SQLException e) {
+
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		catch(ParseException e){
-			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
-		
+
 	}
 }
